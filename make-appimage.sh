c5ce868769d5981a46ae4dcd5b1a03b169e5289e
Link4Electronics <link4electronics@gmail.com>
2026-03-31 19:42:08 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -13,7 +13,7 @@ export DEPLOY_OPENGL=1
 export DEPLOY_VULKAN=1
 
 # Deploy dependencies
-quick-sharun ./AppDir/bin/*
+quick-sharun ./AppDir/bin/webamp-desktop #./AppDir/bin/*
 
 # Additional changes can be done in between here
 
